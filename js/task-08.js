01332e0f2ef77aa1410ec935b9c7898818c37054
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2023-04-11 22:47:13 +0300

    completesd task-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", handleSubmit)
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const login = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    ;
+    if (login === "" || password === "") {
+        alert("All fields must be filled")
+    }
+
+    const formData = {
+        login,
+        password,
+    }
+
+    console.log(formData);
+    event.currentTarget.reset();
+
+
+}
