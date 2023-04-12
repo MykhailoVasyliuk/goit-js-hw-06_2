dff0a2c8108453daf691d379655d7a0c31a2d9f4
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2023-04-12 22:08:50 +0300

    completed task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyRef = document.querySelector("body");
+const changeColorButtonRef = document.querySelector(".change-color");
+const colorRef = document.querySelector(".color");
+
+changeColorButtonRef.addEventListener('click', toChangeColor)
+
+function toChangeColor() {
+  const currentColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = currentColor;
+  colorRef.textContent = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
